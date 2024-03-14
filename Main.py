a46a8051a9c05ed4cef1442409925cd11605db0e
Jimbo <happycoder@foxmail.com>
2024-03-14 17:52:02 +0800

    ·

--- Main.py
+++ Main.py
@@ -1,57 +1,72 @@
-from keras.layers import Input, Dense, LSTM, merge ,Conv1D,Dropout,Bidirectional,Multiply
-from keras.models import Model
-
-
+# 本导入顺序可以看到类型
 from attention_utils import get_activations
-from keras.layers import merge
-from keras.layers.core import *
-from keras.layers.recurrent import LSTM
-from keras.models import *
+import tensorflow as tf
+# import tensorflow_docs
+import datetime
+import os
+import pandas as pd
+import numpy as np
 
-import  pandas as pd
-import  numpy as np
 from sklearnex import patch_sklearn
+from matplotlib import pyplot as plt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble, metrics
+
+# from tensorflow import keras
+import keras
+from keras import layers
+from keras import models
+
+from myK import *
+
+# from keras.layers import merge
+# from keras.layers.core import *
+# from keras.layers.recurrent import LSTM
+# from keras.models import *
+
 patch_sklearn()
 
 
-
-
-
 SINGLE_ATTENTION_VECTOR = False
+
+
 def attention_3d_block(inputs):
     # inputs.shape = (batch_size, time_steps, input_dim)
     input_dim = int(inputs.shape[2])
     a = inputs
-    #a = Permute((2, 1))(inputs)
-    #a = Reshape((input_dim, TIME_STEPS))(a) # this line is not useful. It's just to know which dimension is what.
-    a = Dense(input_dim, activation='softmax')(a)
+    # a = Permute((2, 1))(inputs)
+    # a = Reshape((input_dim, TIME_STEPS))(a) # this line is not useful. It's just to know which dimension is what.
+    a = layers.Dense(input_dim, activation='softmax')(a)
     if SINGLE_ATTENTION_VECTOR:
-        a = Lambda(lambda x: K.mean(x, axis=1), name='dim_reduction')(a)
-        a = RepeatVector(input_dim)(a)
-    a_probs = Permute((1, 2), name='attention_vec')(a)#维数转置
+        a = layers.Lambda(lambda x: K.mean(x, axis=1), name='dim_reduction')(a)
+        a = layers.RepeatVector(input_dim)(a)
+    a_probs = layers.Permute((1, 2), name='attention_vec')(a)  # 维数转置
 
-    output_attention_mul = merge([inputs, a_probs], name='attention_mul', mode='mul')#把两个矩阵拼接
+    output_attention_mul = layers.concatenate(
+        [inputs, a_probs], axis=-1)  # 把两个矩阵拼接
     return output_attention_mul
 
 # 注意力机制的另一种写法 适合上述报错使用 来源:https://blog.csdn.net/uhauha2929/article/details/80733255
+
+
 def attention_3d_block2(inputs, single_attention_vector=False):
     # 如果上一层是LSTM，需要return_sequences=True
     # inputs.shape = (batch_size, time_steps, input_dim)
     time_steps = K.int_shape(inputs)[1]
     input_dim = K.int_shape(inputs)[2]
-    a = Permute((2, 1))(inputs)
-    a = Dense(time_steps, activation='softmax')(a)
+    a = layers.Permute((2, 1))(inputs)
+    a = layers.Dense(time_steps, activation='softmax')(a)
     if single_attention_vector:
-        a = Lambda(lambda x: K.mean(x, axis=1))(a)
-        a = RepeatVector(input_dim)(a)
+        a = layers.Lambda(lambda x: K.mean(x, axis=1))(a)
+        a = layers.RepeatVector(input_dim)(a)
 
-    a_probs = Permute((2, 1))(a)
+    a_probs = layers.Permute((2, 1))(a)
     # 乘上了attention权重，但是并没有求和，好像影响不大
     # 如果分类任务，进行Flatten展开就可以了
     # element-wise
-    output_attention_mul = Multiply()([inputs, a_probs])
+    output_attention_mul = layers.Multiply()([inputs, a_probs])
     return output_attention_mul
-
 
 
 def create_dataset(dataset, look_back):
@@ -60,77 +75,80 @@ def create_dataset(dataset, look_back):
     '''
     dataX, dataY = [], []
     for i in range(len(dataset)-look_back-1):
-        a = dataset[i:(i+look_back),:]
+        a = dataset[i:(i+look_back), :]
         dataX.append(a)
-        dataY.append(dataset[i + look_back,:])
+        dataY.append(dataset[i + look_back, :])
     TrainX = np.array(dataX)
     Train_Y = np.array(dataY)
 
     return TrainX, Train_Y
 
-#多维归一化  返回数据和最大最小值
+# 多维归一化  返回数据和最大最小值
+
+
 def NormalizeMult(data):
-    #normalize 用于反归一化
+    # normalize 用于反归一化
     data = np.array(data)
-    normalize = np.arange(2*data.shape[1],dtype='float64')
+    normalize = np.arange(2*data.shape[1], dtype='float64')
 
-    normalize = normalize.reshape(data.shape[1],2)
+    normalize = normalize.reshape(data.shape[1], 2)
     print(normalize.shape)
-    for i in range(0,data.shape[1]):
-        #第i列
-        list = data[:,i]
-        listlow,listhigh =  np.percentile(list, [0, 100])
+    for i in range(0, data.shape[1]):
+        # 第i列
+        list = data[:, i]
+        listlow, listhigh = np.percentile(list, [0, 100])
         # print(i)
-        normalize[i,0] = listlow
-        normalize[i,1] = listhigh
+        normalize[i, 0] = listlow
+        normalize[i, 1] = listhigh
         delta = listhigh - listlow
         if delta != 0:
-            #第j行
-            for j in range(0,data.shape[0]):
-                data[j,i]  =  (data[j,i] - listlow)/delta
-    #np.save("./normalize.npy",normalize)
-    return  data,normalize
+            # 第j行
+            for j in range(0, data.shape[0]):
+                data[j, i] = (data[j, i] - listlow)/delta
+    # np.save("./normalize.npy",normalize)
+    return data, normalize
 
-#多维反归一化
-def FNormalizeMult(data,normalize):
+# 多维反归一化
+
+
+def FNormalizeMult(data, normalize):
     data = np.array(data)
-    for i in  range(0,data.shape[1]):
-        listlow =  normalize[i,0]
-        listhigh = normalize[i,1]
+    for i in range(0, data.shape[1]):
+        listlow = normalize[i, 0]
+        listhigh = normalize[i, 1]
         delta = listhigh - listlow
         if delta != 0:
-            #第j行
-            for j in range(0,data.shape[0]):
-                data[j,i]  =  data[j,i]*delta + listlow
+            # 第j行
+            for j in range(0, data.shape[0]):
+                data[j, i] = data[j, i]*delta + listlow
 
     return data
 
 
 def attention_model():
-    inputs = Input(shape=(TIME_STEPS, INPUT_DIMS))
+    inputs = layers.Input(shape=(TIME_STEPS, INPUT_DIMS))
 
-    x = Conv1D(filters = 64, kernel_size = 1, activation = 'relu')(inputs)  #, padding = 'same'
-    x = Dropout(0.3)(x)
+    x = layers.Conv1D(filters=64, kernel_size=1, activation='relu')(
+        inputs)  # , padding = 'same'
+    x = layers.Dropout(0.3)(x)
 
-    #lstm_out = Bidirectional(LSTM(lstm_units, activation='relu'), name='bilstm')(x)
-    #对于GPU可以使用CuDNNLSTM
-    lstm_out = Bidirectional(LSTM(lstm_units, return_sequences=True))(x)
-    lstm_out = Dropout(0.3)(lstm_out)
+    # lstm_out = Bidirectional(LSTM(lstm_units, activation='relu'), name='bilstm')(x)
+    # 对于GPU可以使用CuDNNLSTM
+    lstm_out = layers.Bidirectional(
+        layers.LSTM(lstm_units, return_sequences=True))(x)
+    lstm_out = layers.Dropout(0.3)(lstm_out)
     attention_mul = attention_3d_block(lstm_out)
-    attention_mul = Flatten()(attention_mul)
+    attention_mul = layers.Flatten()(attention_mul)
 
-    output = Dense(1, activation='sigmoid')(attention_mul)
-    model = Model(inputs=[inputs], outputs=output)
+    output = layers.Dense(1, activation='sigmoid')(attention_mul)
+    model = models.Model(inputs=[inputs], outputs=output)
     return model
 
 
-
-
-#加载数据
-
+# 加载数据
 data = pd.read_csv("./pollution.csv")
 
-data = data.drop(['date','wnd_dir'], axis = 1)
+data = data.drop(['date', 'wnd_dir'], axis=1)
 
 print(data.columns)
 print(data.shape)
@@ -140,19 +158,19 @@ INPUT_DIMS = 7
 TIME_STEPS = 20
 lstm_units = 64
 
-#归一化
-data,normalize = NormalizeMult(data)
-pollution_data = data[:,0].reshape(len(data),1)
+# 归一化
+data, normalize = NormalizeMult(data)
+pollution_data = data[:, 0].reshape(len(data), 1)
 
-train_X, _ = create_dataset(data,TIME_STEPS)
-_ , train_Y = create_dataset(pollution_data,TIME_STEPS)
+train_X, _ = create_dataset(data, TIME_STEPS)
+_, train_Y = create_dataset(pollution_data, TIME_STEPS)
 
-print(train_X.shape,train_Y.shape)
+print(train_X.shape, train_Y.shape)
 
 m = attention_model()
 m.summary()
 m.compile(optimizer='adam', loss='mse')
 m.fit([train_X], train_Y, epochs=10, batch_size=64, validation_split=0.1)
-#m.save("./model.h5")
-#np.save("normalize.npy",normalize)
+# m.save("./model.h5")
+# np.save("normalize.npy",normalize)
 print("done")
---
+++ myK/__init__.py
@@ -0,0 +1,9 @@
+
+
+
+
+import tensorflow as tf
+
+from keras import backend 
+
+__all__ = ['tf', 'backend']
